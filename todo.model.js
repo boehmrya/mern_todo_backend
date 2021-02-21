775727721c18ad564ececf10ba0246a3829e3682
Ryan <ryan.w.boehm@gmail.com>
2021-02-21 11:03:50 -0500

    adjusted the schema

--- todo.model.js
+++ todo.model.js
@@ -8,20 +8,25 @@ const Todo = new Schema({
     todo_completed: Boolean
 });
 
-const Project = Schema({
+/*
+const Project = new Schema({
   title: String,
   description: String,
   tasks: [{ type: Schema.Types.ObjectId, ref: 'Todo' }],
   people: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
 });
 
-const Person = Schema({
+const Person = new Schema({
   name: String,
   bio: String,
   projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }],
   tasks: [{ type: Schema.Types.ObjectId, ref: 'Todo' }]
 });
+*/
 
 module.exports = mongoose.model('Todo', Todo);
+
+/*
 module.exports = mongoose.model('Project', Project);
 module.exports = mongoose.model('Person', Person);
+*/
